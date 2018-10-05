3b1879b4e3e12a4ed42ac654cc50d1f369f5e02b
maikel buunen <maikelbuunen2000@outlook.com>
2018-10-05 11:53:51 +0200

    unity project added

---
+++ Dreambound/TwinStickShooter/Assets/Resources/Scripts/Player/GearAnim.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearAnim : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(!GameManager.useController)
+        {
+            anim.SetFloat("VelocityX", Input.GetAxis("Horizontal"));
+            anim.SetFloat("VelocityZ", Input.GetAxis("Vertical"));
+        }
+
+        if(GameManager.useController)
+        {
+            anim.SetFloat("VelocityX", Input.GetAxis("LHorizontal"));
+            anim.SetFloat("VelocityZ", Input.GetAxis("LVertical"));
+        }
+    }
+}
---
+++ Dreambound/TwinStickShooter/Assets/Resources/Scripts/System/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering.PostProcessing;
+
+public class GameManager : MonoBehaviour
+{
+    private Camera cam;
+
+    public static bool useController;
+    [SerializeField] Toggle toggle;
+
+    [Header("Parents")]
+    [SerializeField] Transform floraParent;
+
+    private void Start()
+    {
+        if(cam == null)
+        {
+            Debug.LogError("Camera has not been set up!");
+        }
+
+        useController = toggle.isOn;
+    }
+
+    public void SetController(bool b)
+    {
+        useController = b;
+    }
+
+    public void CleanScene()
+    {
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag("Flora"))
+        {
+            g.transform.parent = floraParent;
+        }
+    }
+
+    public void SetCamera()
+    {
+        cam = Camera.main;
+
+        cam.fieldOfView = 65f;
+        cam.backgroundColor = Color.black;
+        cam.clearFlags = CameraClearFlags.SolidColor;
+        cam.gameObject.AddComponent<PostProcessVolume>();
+    }
+
+}
